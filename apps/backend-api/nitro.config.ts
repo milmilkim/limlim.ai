27dfb7f34f58ea3bad1279feb2fa06f1d11084e5
sori kim <soundkim27@gmail.com>
2025-08-06 00:38:44 +0900

    chore

--- apps/backend-api/nitro.config.ts
+++ apps/backend-api/nitro.config.ts
@@ -1,8 +1,21 @@
+import { resolve } from "path";
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: "server",
-  preset: 'vercel',
+  preset: "vercel",
+  output: {
+    dir: resolve(__dirname, "../../.vercel/output"),
+  },
+  compatibilityDate: "2025-08-06",
   experimental: {
-    wasm: true
-  }
+    wasm: true,
+  },
+  publicAssets: [
+    {
+      dir: resolve(__dirname, "../web-client/dist"),
+      baseURL: "/",
+      maxAge: 60 * 60 * 24 * 365,
+    },
+  ],
 });
